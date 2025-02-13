5103d502123c53e9ab8ac0ab1c8732a4cac343b7
PlatinumPixel <96799498+PlatinumPixel@users.noreply.github.com>
2025-02-13 22:37:17 +0200

    Vektorius implementuota bet studentu ir pazymiu kiekio
    Pridetas h failas ir vektoriai bet mokiniu skaiciaus veikia :)

---
+++ bibl.h
@@ -0,0 +1,17 @@
+# ifndef BIBL_H
+# define BIBL_H
+
+#include <iostream> 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using std::cin; 
+using std::cout;
+using std::endl;
+using std::string;  
+using std::vector;
+using std::setw;
+
+
+# endif
---
+++ vektoriai.cpp
@@ -0,0 +1,81 @@
+#include "bibl.h"
+
+struct stud{
+    string vard;
+    string pava;
+    vector <int> tarp;
+    double tarpsum=0;
+    double tarpvid;
+    double tarpmed;
+    double egz;
+    double galutinisvid;
+    double galutinismed;
+};
+
+vector <stud> A;
+
+void spausdina(vector <stud> A);
+void rankinis(vector <stud> &A);
+void pazymiai(stud &temp);
+
+int main(){  
+    setlocale(LC_ALL, "Lithuanian");
+    char input;
+    cout << "Iveskite Y kad irasyti studenta, arba N kad sustoti ir atspausdinti ";
+    while ((cin >> input)){
+        if (input=='Y'){
+            rankinis(A);
+            cout << "Iveskite Y kad irasyti studenta, arba N kad sustoti ir atspausdinti " << endl;
+            continue;
+        }
+        if (input=='N'){
+            spausdina(A);
+            break;
+        }
+        cout << "Ivedete neteisinga simobli, pabandykit vel! :)" << endl;
+        cout << "Iveskite Y kad irasyti studenta, arba N kad sustoti ir atspausdinti " << endl;
+    }
+ return 0;
+}
+
+void rankinis(vector <stud> &A){
+    stud temp;  
+
+        cout << "Iveskite studento Varda ir pavarde ";
+        cin >> temp.vard >> temp.pava;
+        cout << "Veskite studento namu darbo pazymius arba neskaiciu, kad daugiau pazymiu nerasyti ";
+        pazymiai(temp);
+        std::sort(temp.tarp.begin(),temp.tarp.end());
+        cout << "Iveskite studento egzamino rezultata ";
+        cin >> temp.egz;
+
+        temp.tarpvid=double(temp.tarpsum/temp.tarp.size());
+        if (temp.tarp.size()%2==0){
+            temp.tarpmed=(temp.tarp[(temp.tarp.size()/2)-1]+temp.tarp[(temp.tarp.size()/2)])/2;
+        }
+        else temp.tarpmed=temp.tarp[(temp.tarp.size()/2)];
+
+        temp.galutinisvid=(temp.tarpvid*0.4)+(temp.egz*0.6);
+        temp.galutinismed=temp.tarpmed*0.4+temp.egz*0.6;
+        A.push_back(temp);    
+}
+
+void spausdina(vector <stud> A){
+        cout << "Pavarde    Vardas      Galutinis(vid.) / Galutinis(med.)" << endl;
+        cout << "----------------------------------------------------" << endl;
+    for (int i=0;i<A.size();i++){
+        cout << std::left << setw(12) << A[i].pava << setw(15) << A[i].vard  << setw(16) << A[i].galutinisvid << " " << A[i].galutinismed << endl;
+    }
+
+}
+
+void pazymiai(stud &temp){
+    int input;
+    string trash;
+    while (cin >> input){
+        temp.tarp.push_back(input);
+        temp.tarpsum+=input;
+    }
+    cin.clear();
+    cin.ignore();
+}
